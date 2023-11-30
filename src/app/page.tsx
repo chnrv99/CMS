a8542063d49b8569d8f4cf60ce9c6ad4ecece275
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-11-30 14:33:32 +0530

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,96 @@
 
 
-import { Courses } from '@/components/Courses'
+import Courses from '@/components/Courses'
 import Footer from '@/components/Footer'
 
 import Navbar from '@/components/Navbar'
 
 
 import Image from 'next/image'
+
+const courses_year_1 = [
+  {
+    title: 'Mathematics 1',
+    course_code: 'MAT101',
+    course_description: 'This course is about the basics of mathematics -1',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 2',
+    course_code: 'MAT102',
+    course_description: 'This course is about the basics of mathematics -2',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 3',
+    course_code: 'MAT103',
+    course_description: 'This course is about the basics of mathematics-3',
+    link: 'https://www.google.com'
+  },
+]
+
+const courses_year_2 = [
+  {
+    title: 'Mathematics 4',
+    course_code: 'MAT104',
+    course_description: 'This course is about the basics of mathematics-4',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 5',
+    course_code: 'MAT105',
+    course_description: 'This course is about the basics of mathematics-5',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 6',
+    course_code: 'MAT106',
+    course_description: 'This course is about the basics of mathematic-6',
+    link: 'https://www.google.com'
+  },
+]
+
+const courses_year_3 = [
+  {
+    title: 'Mathematics 7',
+    course_code: 'MAT107',
+    course_description: 'This course is about the basics of mathematics-7',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 8',
+    course_code: 'MAT108',
+    course_description: 'This course is about the basics of mathematics-8',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 9',
+    course_code: 'MAT109',
+    course_description: 'This course is about the basics of mathematics-9',
+    link: 'https://www.google.com'
+  },
+]
+
+const courses_year_4 = [
+  {
+    title: 'Mathematics 10',
+    course_code: 'MAT110',
+    course_description: 'This course is about the basics of mathematics-10',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 11',
+    course_code: 'MAT111',
+    course_description: 'This course is about the basics of mathematics-11',
+    link: 'https://www.google.com'
+  },
+  {
+    title: 'Mathematics 12',
+    course_code: 'MAT112',
+    course_description: 'This course is about the basics of mathematics-12',
+    link: 'https://www.google.com'
+  },
+]
 
 export default function Home() {
 
@@ -27,11 +111,9 @@ export default function Home() {
             <div className="container">
               <h1 className="text-xl">Year 1</h1>
               <div className="container flex">
-
-                <Courses />
-                <Courses />
-                
-                <Courses />
+                {courses_year_1.map((data, index) => (
+                  <Courses key={index} data={data} />
+                ))}
               </div>
             </div>
             <br />
@@ -40,10 +122,10 @@ export default function Home() {
               <h1 className="text-xl">Year 2</h1>
               <div className="container flex">
 
-                <Courses />
-                <Courses />
-                <Courses />
-          
+                {courses_year_2.map((data, index) => (
+                  <Courses key={index} data={data} />
+                ))}
+
               </div>
 
             </div>
@@ -53,10 +135,9 @@ export default function Home() {
               <h1 className="text-xl">Year 3</h1>
               <div className="container flex">
 
-                <Courses />
-                <Courses />
-               
-                <Courses />
+                {courses_year_3.map((data, index) => (
+                  <Courses key={index} data={data} />
+                ))}
               </div>
             </div>
             <br />
@@ -65,10 +146,10 @@ export default function Home() {
               <h1 className="text-xl">Year 4</h1>
               <div className="container flex">
 
-                <Courses />
-                <Courses />
-                <Courses />
-              
+                {courses_year_4.map((data, index) => (
+                  <Courses key={index} data={data} />
+                ))}
+
               </div>
             </div>
             <br />
--- src/components/Courses.tsx
+++ src/components/Courses.tsx
@@ -3,76 +3,89 @@ import { BellIcon, CheckIcon } from "@radix-ui/react-icons"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import { Switch } from "@/components/ui/switch"
+import Link from "next/link"
 
 const notifications = [
-  {
-    title: "Your call has been confirmed.",
-    description: "1 hour ago",
-  },
-  {
-    title: "You have a new message!",
-    description: "1 hour ago",
-  },
-  {
-    title: "Your subscription is expiring soon!",
-    description: "2 hours ago",
-  },
+    {
+        title: "Your call has been confirmed.",
+        description: "1 hour ago",
+    },
+    {
+        title: "You have a new message!",
+        description: "1 hour ago",
+    },
+    {
+        title: "Your subscription is expiring soon!",
+        description: "2 hours ago",
+    },
 ]
+
+
+
+
 
 type CardProps = React.ComponentProps<typeof Card>
 
-export function Courses({ className, ...props }: CardProps) {
-  return (
-    <Card className={cn("w-[380px]", className)} {...props}>
-      <CardHeader>
-        <CardTitle>Notifications</CardTitle>
-        <CardDescription>You have 3 unread messages.</CardDescription>
-      </CardHeader>
-      <CardContent className="grid gap-4">
-        <div className=" flex items-center space-x-4 rounded-md border p-4">
-          <BellIcon />
-          <div className="flex-1 space-y-1">
-            <p className="text-sm font-medium leading-none">
-              Push Notifications
-            </p>
-            <p className="text-sm text-muted-foreground">
-              Send notifications to device.
-            </p>
-          </div>
-          <Switch />
-        </div>
-        <div>
-          {notifications.map((notification, index) => (
-            <div
-              key={index}
-              className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-            >
-              <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-              <div className="space-y-1">
-                <p className="text-sm font-medium leading-none">
-                  {notification.title}
-                </p>
-                <p className="text-sm text-muted-foreground">
-                  {notification.description}
-                </p>
-              </div>
-            </div>
-          ))}
-        </div>
-      </CardContent>
-      <CardFooter>
-        <Button className="w-full">
-          <CheckIcon className="mr-2 h-4 w-4" /> Mark all as read
-        </Button>
-      </CardFooter>
-    </Card>
-  )
+export default function Courses({ className, ...props }: any) {
+    console.log(props)
+    return (
+        <Card className={cn("w-auto h-auto hover:bg-slate-900 hover:text-slate-200 transition-colors", className)} {...props}>
+            <CardHeader>
+                <div className="flex justify-between">
+                    <CardTitle className="flex">{props.data.title}</CardTitle>
+
+
+                </div>
+            </CardHeader>
+            <CardContent className="grid gap-4">
+
+                <div>
+
+                    <div
+
+                        className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                    >
+                        <span className="flex h-2 w-2 translate-y-1 rounded-full" />
+                        <div className="space-y-1">
+                            <p className="text-xl font-medium leading-none text-blue-100">
+                                {props.data.course_code}
+                            </p>
+
+                        </div>
+                    </div>
+
+                    <div
+
+                        className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
+                    >
+                        <span className="flex h-2 w-2 translate-y-1 rounded-full" />
+                        <div className="space-y-1">
+
+                            <p className="text-sm text-muted-foreground">
+                                {props.data.course_description}
+                            </p>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </CardContent>
+            <CardFooter>
+                <Link href={props.data.link}>
+                    <Button className="w-full">
+                       Go to course
+                    </Button>
+                </Link>
+            </CardFooter>
+        </Card>
+    )
 }
