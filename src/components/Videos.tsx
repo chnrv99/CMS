f7dd85a45cbab5257a7535c7f43f802c1d0bc1bc
chnrv99 <nishanth.c2021@vitstudent.ac.in>
2023-12-08 01:01:14 +0530

    added scroll

--- src/components/Videos.tsx
+++ src/components/Videos.tsx
@@ -1,25 +1,34 @@
 import Link from 'next/link'
 import React from 'react'
 
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
+
 
 
 export default function Videos(props: any) {
     let data = props.data
-    console.log("the data is",data)
+    console.log("the data is", data)
     let handleVideoClick = props.handleVideoClick
     return (
-        <div className="container">
-            {data.videos.map((data: any, index: any) => (
 
-                <div key={index}>
+        <ScrollArea className="h-72 w-48 rounded-md border">
 
-                    <div className="text-xl">
-                        {data.video_title}
+            <div className="container">
+                {data.videos.map((data: any, index: any) => (
+
+                    <div key={index}>
+
+                        <div className="text-xl">
+                            {data.video_title}
+                        </div>
+                        <div onClick={() => handleVideoClick(data)} className='underline'>Watch Video</div>
                     </div>
-                    <div onClick={()=> handleVideoClick(data)} className='underline'>Watch Video</div>
-                </div>
 
-            ))}
-        </div>
+                ))}
+            </div>
+            <Separator className='my-2' />
+        </ScrollArea>
+
     )
 }
